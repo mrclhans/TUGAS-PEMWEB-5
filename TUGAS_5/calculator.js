5a1c0495116da8effe7ad29d61437a63d7d6a227
Cellosgabotak17 <115338336+mrclhans@users.noreply.github.com>
2024-03-28 13:56:05 +0700

    Add files via upload

---
+++ TUGAS_5/calculator.js
@@ -0,0 +1,18 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculate() {
+    var displayValue = document.getElementById('display').value;
+    var result;
+    try {
+      result = eval(displayValue);
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+}
+}
